9a111b4ddb092199ebbf3a1acc1c258f2e95359c
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2024-01-11 14:42:18 +0700

    Menambahkan HalamanEditKeluarga.kt

--- app/src/main/java/com/example/sensuspenduduk/ui/halaman/HalamanEditKeluarga.kt
+++ app/src/main/java/com/example/sensuspenduduk/ui/halaman/HalamanEditKeluarga.kt
@@ -1,11 +1,81 @@
 package com.example.sensuspenduduk.ui.halaman
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.sensuspenduduk.R
+import com.example.sensuspenduduk.data.Keluarga
 import com.example.sensuspenduduk.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
 
-object DestinasiDetail : DestinasiNavigasi {
-    override val route = "item_details"
-    override val titleRes = R.string.detail_Keluarga
-    const val keluargaIdArg = "itemId"
-    val routeWithArgs = "$route/{$keluargaIdArg}"
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_Keluarga
+    const val kkIdArg = "itemId"
+    val routeWithArgs = "$route/{$kkIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            KeluargaTopAppBar(
+                title = stringResource(id = DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp,
+                scrollBehavior = scrollBehavior
+            )
+        },
+    ) { innerPadding ->
+        EntryKeluargaBody(
+            uiStateKeluarga = viewModel.keluargaUiState,
+            onKeluargaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateKeluarga()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+
+    }
 }
