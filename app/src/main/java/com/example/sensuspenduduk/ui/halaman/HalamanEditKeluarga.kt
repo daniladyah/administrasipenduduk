3a7c7899feadac7a42d667d1ffcce311ad0992e7
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2024-01-11 14:25:41 +0700

    Membuat File HalamanEditKeluarga.kt

--- app/src/main/java/com/example/sensuspenduduk/ui/halaman/HalamanEditKeluarga.kt
+++ app/src/main/java/com/example/sensuspenduduk/ui/halaman/HalamanEditKeluarga.kt
@@ -1,2 +1,11 @@
 package com.example.sensuspenduduk.ui.halaman
 
+import com.example.sensuspenduduk.R
+import com.example.sensuspenduduk.navigasi.DestinasiNavigasi
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_details"
+    override val titleRes = R.string.detail_Keluarga
+    const val keluargaIdArg = "itemId"
+    val routeWithArgs = "$route/{$keluargaIdArg}"
+}
