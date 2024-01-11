52dabd3eba92f5b7bfa6d362894a581d9989b376
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2024-01-11 15:10:40 +0700

    menambahkan edit di PenyediaViewModel

--- app/src/main/java/com/example/sensuspenduduk/ui/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/sensuspenduduk/ui/model/PenyediaViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.sensuspenduduk.ui.model
 
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.sensuspenduduk.AplikasiKeluarga
@@ -10,5 +12,22 @@ object PenyediaViewModel {
         initializer {
             HomeViewModel(AplikasiKeluarga().container.repositoriKeluarga)
         }
+        initializer { EntryViewModel(aplikasiKeluarga().container.repositoriKeluarga) }
+        initializer {
+            EditViewModel(
+                createSavedStateHandle(),
+                aplikasiKeluarga().container.repositoriKeluarga
+            )
+        }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiKeluarga().container.repositoriKeluarga
+            )
+        }
+
     }
 }
+
+fun CreationExtras.aplikasiKeluarga(): AplikasiSensus =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSensus)
